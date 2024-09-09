ed6bdc8d34be36c4669f146ba85dfdc37c9eb407
Abdul Rehman Khalid <59740060+Archeema16@users.noreply.github.com>
2024-09-09 21:47:34 +0300

    Properties Energy retrieval (#2)
    * feat,docs: Properties Energy Table Creation
    
    * Properties energy data retrieval

--- schema.sql
+++ schema.sql
@@ -5,3 +5,21 @@ CREATE TABLE IF NOT EXISTS properties (
         property_name VARCHAR(100) NULL,
         property_code VARCHAR(100) NULL
     );
+
+
+CREATE TABLE IF NOT EXISTS properties_daily_energy (
+    id SERIAL PRIMARY KEY,
+    location_name VARCHAR(255) NOT NULL,
+    reporting_group VARCHAR(20) NOT NULL,
+    value DOUBLE PRECISION,
+    unit VARCHAR(10),
+    date TIMESTAMP,
+    load_date DATE  DEFAULT CURRENT_DATE,
+    version VARCHAR(10),
+    response_error JSONB
+);
+
+CREATE INDEX IF NOT EXISTS idx_location_name ON properties_daily_energy (location_name);
+CREATE INDEX IF NOT EXISTS idx_reporting_group ON properties_daily_energy (reporting_group);
+CREATE INDEX IF NOT EXISTS idx_date ON properties_daily_energy (date);
+CREATE INDEX IF NOT EXISTS idx_location_date ON properties_daily_energy (location_name,date);
